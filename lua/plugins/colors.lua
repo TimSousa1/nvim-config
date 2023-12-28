1cc53db0392b6e08b9e564305fac231cf72170d2
TimSousa <martimnevessousa@gmail.com>
2023-12-28 21:31:40 +0000

    update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -4,6 +4,12 @@ return {
         name = "catppuccin",
         lazy = false,
         priority = 1000,
+        dependencies = {
+            "nvim-lualine/lualine.nvim"
+        },
+        integrations = {
+            mason = true,
+        },
         config = function()
             vim.cmd([[colorscheme catppuccin-mocha]])
         end,
